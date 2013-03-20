98215dd754e2e6158724492fec778d8505361681
OpenShift guest <woodie41@hotmail.com>
2013-03-20 00:29:41 -0400

    Set up test guidelines and included assert tests for add

--- Homework-9.1.php
+++ Homework-9.1.php
@@ -27,17 +27,17 @@
 
 function add($a, $b)
 {
-    return 2;
+    return $a + $b;
 }
 
     assert(add(+1, +1) == +2);
+    assert(add(+1, -1) == +0);
+    assert(add(+1, +0) == +1);
+    assert(add(+0, +1) == +1);
+    assert(add(+0, -1) == -1);
+    assert(add(+0, +0) == +0);
+    assert(add(-1, +1) == +0);  
+    assert(add(-1, -1) == -2);
+    assert(add(-1, +0) == -1);
     
     
-
-
-
-
-
-
-
-
