b3fa62401cb66502ab3a7ba0fe25c708f905ce17
OpenShift guest <woodie41@hotmail.com>
2013-03-26 19:52:39 -0400

    This was a failing assert test. Will correct

---
+++ Homework-10.1.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * four functions with two parameters:
+ * - addition
+ * - subtraction
+ * - multiplication
+ * - division
+ * 
+ * Must accept positive and negative Numbers (int or 
+ * float) AND zero. Must not make an error!
+ */
+
+
+/**
+ * math | one | two | value
+ * ===+=====+=====+======
+ * +    | +1  | +1  | +2
+ * +    | +1  | -1  | +0
+ * +    | +1  | +0  | +1
+ * +    | +0  | +1  | +1
+ * +    | +0  | -1  | -1
+ * +    | +0  | +0  | +0
+ * +    | -1  | +1  | +0
+ * +    | -1  | -1  | -2
+ * +    | -1  | +0  | -1
+ */
+ 
+ assert(add(+1, +1) == 2);
