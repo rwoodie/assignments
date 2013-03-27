bb772509577b1a40e5c3427d7f6ed807e0fbc743
OpenShift guest <woodie41@hotmail.com>
2013-03-26 20:11:50 -0400

    Completed the remaining assert tests for addition

--- Homework-10.1.php
+++ Homework-10.1.php
@@ -31,8 +31,15 @@ function add($a, $b)
     return $a + $b;
 }
  
+{
     assert(add(+1, +1) == +2);
     assert(add(+1, -1) == +0);
-    
+    assert(add(+1, +0) == +1);
+    assert(add(+0, +1) == +1);
+    assert(add(+0, -1) == -1);
+    assert(add(+0, +0) == +0);
+    assert(add(-1, +1) == +0);
+    assert(add(-1, -1) == -2);
+    assert(add(-1, +0) == -1);
+}
 
- 
