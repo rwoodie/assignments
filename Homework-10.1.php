698d5c335807f7339aa499e18b2d8ca4800a2b99
OpenShift guest <woodie41@hotmail.com>
2013-03-27 22:23:34 -0400

    got a failing test for the first assert test

--- Homework-10.1.php
+++ Homework-10.1.php
@@ -10,65 +10,21 @@
  * float) AND zero. Must not make an error!
  */
 
-$inputs = array(
-    
-    array(+1, +1)
-    array(+1, -1)
-    array(+1, +0)
-    array(+0, +1)
-    array(+0, -1)
-    array(+0, +0)
-    array(-1, +1)
-    array(-1, -1)
-    array(-1, +0)
-    )
 
 /**
  * math | one | two | value
  * ===+=====+=====+======
- * +    | +1  | +1  | +2
- * +    | +1  | -1  | +0
- * +    | +1  | +0  | +1
- * +    | +0  | +1  | +1
- * +    | +0  | -1  | -1
- * +    | +0  | +0  | +0
- * +    | -1  | +1  | +0
- * +    | -1  | -1  | -2
- * +    | -1  | +0  | -1
+ * x    | +1  | +1  | +1
+ * x    | +1  | -1  | -1
+ * x    | +1  | +0  | +0
+ * x    | +0  | +1  | +1
+ * x    | +0  | -1  | -1
+ * x    | +0  | +0  | +0
+ * x    | -1  | +1  | +0
+ * x    | -1  | -1  | -2
+ * x    | -1  | +0  | -1
  */
  
-// This function is for addition
-function add($a, $b)
-{
-    return $a + $b;
-}
- 
-$outputs = array(
-        +2,
-        +0,
-        +1,
-        +1,
-        -1,
-        +0,
-        +0,
-        -2,
-        -1,
-    );
-    
-    for ($i = 0; $i < 9; $i++)
-    {
-        assert(add($inputs[$i][0], $inputs[$i][1]) == $outputs[$i]);
-    }
 
-{
-    assert(add(+1, +1) == +2);
-    assert(add(+1, -1) == +0);
-    assert(add(+1, +0) == +1);
-    assert(add(+0, +1) == +1);
-    assert(add(+0, -1) == -1);
-    assert(add(+0, +0) == +0);
-    assert(add(-1, +1) == +0);
-    assert(add(-1, -1) == -2);
-    assert(add(-1, +0) == -1);
-}
+assert(multiply(+1, +1) == +1)
 
