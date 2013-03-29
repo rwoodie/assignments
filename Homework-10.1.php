867ef2ea9606127044395d9bc725bad1d3947802
OpenShift guest <woodie41@hotmail.com>
2013-03-29 06:50:29 -0400

    input remainin tests for subtraction with passing tests.

--- Homework-10.1.php
+++ Homework-10.1.php
@@ -32,4 +32,10 @@ function subtraction($a, $b)
 
     assert(subtraction(+1, +1) == +0);
     assert(subtraction(+1, -1) == +2);
-    
+    assert(subtraction(+1, +0) == +1);
+    assert(subtraction(+0, +1) == -1);
+    assert(subtraction(+0, -1) == +1);
+    assert(subtraction(+0, +0) == +0);
+    assert(subtraction(-1, +1) == -2);
+    assert(subtraction(-1, -1) == +0);
+    assert(subtraction(-1, +0) == -1);
