b778ef2a97697065e53bbaf41456fdd7ba66b853
OpenShift guest <woodie41@hotmail.com>
2013-04-03 00:02:51 -0400

    Completed the remaining addition tests. Added comment for the assert test. Put curly brackets around tests. got passing test

--- Testfile.php
+++ Testfile.php
@@ -2,7 +2,16 @@
 
 include ("Calculator.php");
 
-assert(addition(+1, +1) == +2);
-assert(addition(+1, -1) == +0);
-
+{
+    //This will test addition in Calculator.php
+    assert(addition(+1, +1) == +2);
+    assert(addition(+1, -1) == +0);
+    assert(addition(+1, +0) == +1);
+    assert(addition(+0, +1) == +1);
+    assert(addition(+0, -1) == -1);
+    assert(addition(+0, +0) == +0);
+    assert(addition(-1, +1) == +0);
+    assert(addition(-1, -1) == -2);
+    assert(addition(-1, +0) == -1);
+}
 
