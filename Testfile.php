883f9aab84cd55ac87fa22dc0dbd74801079e4cc
OpenShift guest <woodie41@hotmail.com>
2013-04-03 21:04:59 -0400

    Added the include function and assert test. Got a failing test

--- Testfile.php
+++ Testfile.php
@@ -2,16 +2,4 @@
 
 include ("Calculator.php");
 
-{
-    //This will test addition in Calculator.php
     assert(addition(+1, +1) == +2);
-    assert(addition(+1, -1) == +0);
-    assert(addition(+1, +0) == +1);
-    assert(addition(+0, +1) == +1);
-    assert(addition(+0, -1) == -1);
-    assert(addition(+0, +0) == +0);
-    assert(addition(-1, +1) == +0);
-    assert(addition(-1, -1) == -2);
-    assert(addition(-1, +0) == -1);
-}
-
