c5a90584c07d0f1980af4969c399f6435a51d1ac
OpenShift guest <woodie41@hotmail.com>
2013-04-03 21:31:29 -0400

    Added the remaining tests for subtraction

--- Testfile.php
+++ Testfile.php
@@ -19,6 +19,13 @@ include ("Calculator.php");
     //This is a test for subtraction
     assert(subtraction(+1, +1) == +0);
     assert(subtraction(+1, -1) == +2);
+    assert(subtraction(+1, +0) == +1);
+    assert(subtraction(+0, +1) == -1);
+    assert(subtraction(+0, -1) == +1);
+    assert(subtraction(+0, +0) == +0);
+    assert(subtraction(-1, +1) == -2);
+    assert(subtraction(-1, -1) == +0);
+    assert(subtraction(-1, +0) == -1);
 }
 
     
