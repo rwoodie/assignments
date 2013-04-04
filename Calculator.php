ba763e566bc8f547f8e3cf0b2a71520113f7603c
OpenShift guest <woodie41@hotmail.com>
2013-04-03 21:08:29 -0400

    Added a function for addition in production file Calculator.php

---
+++ Calculator.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * four functions with two parameters:
+ * - addition
+ * - subtraction
+ * - multiplication
+ * - division
+ * 
+ * Must accept positive and negative Numbers (int or 
+ * float) AND zero. Must not make an error!
+ */
+ 
+ /**
+ * math | one | two | value
+ * ===+=====+=====+======
+ * +    | +1  | +1  | +2
+ * +    | +1  | -1  | +0
+ * +    | +1  | +0  | +1
+ * +    | +0  | +1  | +1
+ * +    | +0  | -1  | -1
+ * +    | +0  | +0  | +0
+ * +    | -1  | +1  | +0
+ * +    | -1  | -1  | -2
+ * +    | -1  | +0  | -1
+ */
+ 
+function addition($a, $b)
+{
+    return +2;
+}
