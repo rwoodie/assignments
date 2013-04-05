229cef57372aef9a75ff681454057ca580785177
OpenShift guest <woodie41@hotmail.com>
2013-04-04 22:33:52 -0400

    got a failing test for first assert test

--- Testfile.php
+++ Testfile.php
@@ -2,30 +2,8 @@
 
 include ("Calculator.php");
 
-{
-    //This is a test for addition
     assert(addition(+1, +1) == +2);
-    assert(addition(+1, -1) == +0);
-    assert(addition(+1, +0) == +1);
-    assert(addition(+0, +1) == +1);
-    assert(addition(+0, -1) == -1);
-    assert(addition(+0, +0) == +0);
-    assert(addition(-1, +1) == +0);
-    assert(addition(-1, -1) == -2);
-    assert(addition(-1, +0) == -1);
-}
 
-{
-    //This is a test for subtraction
-    assert(subtraction(+1, +1) == +0);
-    assert(subtraction(+1, -1) == +2);
-    assert(subtraction(+1, +0) == +1);
-    assert(subtraction(+0, +1) == -1);
-    assert(subtraction(+0, -1) == +1);
-    assert(subtraction(+0, +0) == +0);
-    assert(subtraction(-1, +1) == -2);
-    assert(subtraction(-1, -1) == +0);
-    assert(subtraction(-1, +0) == -1);
-}
+
 
     
