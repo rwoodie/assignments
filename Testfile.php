602ee99d71363a6ad24e7b7eff1996b3b4384ee9
OpenShift guest <woodie41@hotmail.com>
2013-04-10 01:33:04 -0400

    completed remaining addition assert tests. Passing

--- Testfile.php
+++ Testfile.php
@@ -16,4 +16,11 @@ include ("Calculator.php");
 
     assert(addition(+1, +1) == +2);
     assert(addition(+1, -1) == +0);
+    assert(addition(+1, +0) == +1);
+    assert(addition(+0, +1) == +1);
+    assert(addition(+0, -1) == -1);
+    assert(addition(+0, +0) == +0);
+    assert(addition(-1, +1) == +0);
+    assert(addition(-1, -1) == -2);
+    assert(addition(-1, +0) == -1);
     
