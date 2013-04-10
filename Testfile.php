4d93590c11fdd9cdd7628f7c473b4ad7f9dd32a7
OpenShift guest <woodie41@hotmail.com>
2013-04-10 01:40:48 -0400

    got a failing test for first multiplication

--- Testfile.php
+++ Testfile.php
@@ -14,6 +14,7 @@ include ("Calculator.php");
     assert(subtraction(-1, +0) == -1);
 }
 
+{//Testing for addition
     assert(addition(+1, +1) == +2);
     assert(addition(+1, -1) == +0);
     assert(addition(+1, +0) == +1);
@@ -23,4 +24,7 @@ include ("Calculator.php");
     assert(addition(-1, +1) == +0);
     assert(addition(-1, -1) == -2);
     assert(addition(-1, +0) == -1);
+}
+
+    assert(multiplication(+1, +1) == +1);
     
