e3361b98c55be2c2848b4e706bf4dfe010d305b9
OpenShift guest <woodie41@hotmail.com>
2013-04-12 01:28:25 -0400

    refactored to do an array for addition

--- Testfile.php
+++ Testfile.php
@@ -2,6 +2,21 @@
 
 include ("Calculator.php");
 
+{
+    $inputs = array(
+        array(+1, +1),
+        array( +1, -1 ),
+        array( +1, +0 ),
+        array( +0, +1 ),
+        array( +0, -1 ),
+        array( +0, +0 ),
+        array( -1, +1 ),
+        array( -1, -1 ),
+        array( -1, +0 ),
+);
+}
+
+{
     assert(addition(+1, +1) == +2);
     assert(addition(+1, -1) == +0);
     assert(addition(+1, +0) == +1);
@@ -11,5 +26,23 @@ include ("Calculator.php");
     assert(addition(-1, +1) == +0);
     assert(addition(-1, -1) == -2);
     assert(addition(-1, +0) == -1);
+}
     
+{
+    $outputs = array(
+        +2,
+        +0,
+        +1,
+        +1,
+        -1,
+        +0,
+        +0,
+        -2,
+        -1,
+    );
     
+    foreach ( $inputs as $index => $input )
+    {
+        assert(addition($input[0], $input[1]) == @$outputs[$index]);
+    }
+}
