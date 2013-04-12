2d86800dc18138058cf1dc6405a4570307c4c71b
OpenShift guest <woodie41@hotmail.com>
2013-04-12 00:59:21 -0400

    Got failing test for 1rst assert

--- Testfile.php
+++ Testfile.php
@@ -2,39 +2,4 @@
 
 include ("Calculator.php");
 
-{//Testing for subtraction
-    assert(subtraction(+1, +1) == +0);
-    assert(subtraction(+1, -1) == +2);
-    assert(subtraction(+1, +0) == +1);
-    assert(subtraction(+0, +1) == -1);
-    assert(subtraction(+0, -1) == +1);
-    assert(subtraction(+0, +0) == +0);
-    assert(subtraction(-1, +1) == -2);
-    assert(subtraction(-1, -1) == +0);
-    assert(subtraction(-1, +0) == -1);
-}
-
-{//Testing for addition
     assert(addition(+1, +1) == +2);
-    assert(addition(+1, -1) == +0);
-    assert(addition(+1, +0) == +1);
-    assert(addition(+0, +1) == +1);
-    assert(addition(+0, -1) == -1);
-    assert(addition(+0, +0) == +0);
-    assert(addition(-1, +1) == +0);
-    assert(addition(-1, -1) == -2);
-    assert(addition(-1, +0) == -1);
-}
-
-    assert(multiplication(+1, +1) == +1);
-    assert(multiplication(+1, -1) == -1);
-    assert(multiplication(+1, +0) == +0);
-    assert(multiplication(+0, +1) == +0);
-    assert(multiplication(+0, -1) == +0);
-    assert(multiplication(+0, +0) == +0);
-    assert(multiplication(-1, +1) == -1);
-    assert(multiplication(-1, -1) == +1);
-    assert(multiplication(-1, +0) == +0);
-    
-    
-    
