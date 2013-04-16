bbadb988909fa1cdfaac98449a70bbcf0b23838a
OpenShift guest <woodie41@hotmail.com>
2013-04-16 17:21:34 -0400

    Got a passing test for can we make a hallway with 10 doors

---
+++ Homework-12.1.Code.php
@@ -0,0 +1,24 @@
+<?php // PROD FILE
+/**
+ * A hallway has 100 closed doors.
+ * Take a walk down the hallway. Stop at every door and open it.
+ * Take a walk down the hallway. Stop at every _other_ door and close it.
+ * Take a walk down the hallway. Stop at every _third_ door.
+ *      If open, close it. If closed, open it.
+ * Take a walk down the hallway. Stop at every _fourth_ door.
+ *      If open, close it. If closed, open it.
+ * Repeat this pattern 100 times.
+ * What is the state of the hallway?
+ */
+ 
+ /**
+ * Function "createHall()" returns a new representation of a Hallway
+ *
+ * @param integer $size number of doors
+ * @return string of length $size of '0'
+ */
+
+function createHall($size)
+{
+    return '0000000000';
+}
