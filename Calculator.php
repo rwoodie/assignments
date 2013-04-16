b0dd38eb290e364133944db4671e7af01172ff19
OpenShift guest <woodie41@hotmail.com>
2013-04-16 17:18:34 -0400

    got a failing test for hallway with 10 doors

--- Calculator.php
+++ Calculator.php
@@ -10,7 +10,7 @@
  * float) AND zero. Must not make an error!
  */
  
- /**
+/**
  * math | one | two | value
  * ===+=====+=====+======
  * +    | +1  | +1  | +2
@@ -29,23 +29,3 @@ function addition($a, $b)
     return $a + $b;
 }
 
-/**
- * op | one | two | value
- * ===+=====+=====+======
- * x  | +1  | +1  | +1
- * x  | +1  | -1  | -1
- * x  | +1  | +0  | +0
- * x  | +0  | +1  | +0
- * x  | +0  | -1  | +0
- * x  | +0  | +0  | +0
- * x  | -1  | +1  | -1
- * x  | -1  | -1  | +1
- * x  | -1  | +0  | +0
- */
-
-function multiplication($a, $b)
-{
-    return $a * $b;
-}
-
- 
---
+++ Homework-12.1.Test.php
@@ -0,0 +1,31 @@
+<?php // TEST FILE
+require 'Homework-12.1.Code.php';
+
+// test: can we make a hallway with 10 doors?
+assert(strlen(createHall(10)) === 10);
+
+// test: hallway match expected: '0000000000'
+
+
+// call: pass1() on hallway
+
+
+// test: hallway match expected: '1111111111'
+
+
+// call: pass2() on hallway
+
+
+// test: hallway match expected: '1010101010'
+
+
+// call: pass3() on hallway
+
+
+// test: hallway match expected: '1000111000'
+
+
+// call: pass4() on hallway
+//$hallway = pass4($hallway);
+
+// test: can we make a hallway with 100 doors?
