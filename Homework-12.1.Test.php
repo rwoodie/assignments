0a17c08e5bf70a85425cf67c1d4b778cc09375cf
OpenShift guest <woodie41@hotmail.com>
2013-04-17 23:12:13 -0400

    failing test for pass 1 on hallway

--- Homework-12.1.Test.php
+++ Homework-12.1.Test.php
@@ -8,17 +8,14 @@ assert(strlen($hallway) === 10);
 assert($hallway === '0000000000');
 
 // call: pass1() on hallway
-
 // test: hallway match expected: '1111111111'
+assert($hallway === '1111111111');
 
 // call: pass2() on hallway
-
 // test: hallway match expected: '1010101010'
 
 
 // call: pass3() on hallway
-
-
 // test: hallway match expected: '1000111000'
 
 
