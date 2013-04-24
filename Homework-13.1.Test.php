af9d20df0967c45619720b17dad7e4a446e6ee9a
OpenShift guest <woodie41@hotmail.com>
2013-04-24 01:32:36 -0400

    got a failing test for 10 doors closed doors

---
+++ Homework-13.1.Test.php
@@ -0,0 +1,20 @@
+<?php // TEST FILE
+require 'Homework-12.1.Code.php';
+
+// test: hallway(10) == '0000000000'
+assert(hallway(10) == '0000000000');
+
+// test: porter('1') == '0'
+
+// test: porter('0') == '1'
+
+// test: firstPass(hallway(10)) == '1111111111'
+
+
+// test: secondPass(hallway(10)) == '0101010101'
+
+
+// test: thirdPass(hallway(10)) == '0010010010'
+
+
+// test: solution(10) == '1001000010'
