7e73fc54cb92ee7a8a18dc077c1cc0171a3a1b23
OpenShift guest <woodie41@hotmail.com>
2013-04-25 00:15:38 -0400

    passing test for first pass

--- Homework-13.1.Code.php
+++ Homework-13.1.Code.php
@@ -30,6 +30,10 @@ function porter($door)
 
 
 // first pass: function(string $hallway): a new Hallway, 1st state of hallway
+function firstPass($hallway)
+{
+    return str_pad($input = '', strlen($hallway), $pad_string = '1');
+}
 
 // second pass: function(string $hallway): a new Hallway, 2nd state of hallway
 
