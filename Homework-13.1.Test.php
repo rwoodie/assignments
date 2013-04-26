2b0782592e23daa196aa55730144a7aa32c76966
OpenShift guest <woodie41@hotmail.com>
2013-04-25 22:16:46 -0400

    got a failing test for is there a hallway

--- Homework-13.1.Test.php
+++ Homework-13.1.Test.php
@@ -3,18 +3,17 @@ require 'Homework-13.1.Code.php';
 
 // test: hallway(10) == '0000000000'
 assert(hallway(10) == '0000000000');
-assert(hallway(2)  == '00');
 
 
 // test: porter('1') == '0'
-assert(porter('1') == '0');
+
 
 
 // test: porter('0') == '1'
-assert(porter('0') == '1');
+
 
 // test: firstPass(hallway(10)) == '1111111111'
-assert firstPass(hallway(10) == '1111111111');
+
 
 // test: secondPass(hallway(10)) == '0101010101'
 
